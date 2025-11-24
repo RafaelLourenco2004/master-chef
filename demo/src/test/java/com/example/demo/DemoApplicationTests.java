da52d0a429e05c06ece1548f83879d7c16bfb2e2
Rafael <rafael.lourenco816@gmail.com>
2025-11-24 16:48:35 -0300

    .

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringBootConfiguration;
+// import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootConfiguration()
 @ActiveProfiles("test")
 class DemoApplicationTests {
 
